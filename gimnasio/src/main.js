dd18589ca7127bc1da642bef672d8a4412fe6c71
Rodrigo313 <rodrigopascual1@example.com>
2021-06-16 11:46:12 +0200

    Login

--- gimnasio/src/main.js
+++ gimnasio/src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import Router from "vue-router";
 
-createApp(App).use(router).mount('#app')
+import App from "./App";
+import Inicio from "./views/Inicio";
+import Login from "./views/Login";
+import Registro from "./views/Registro";
+
+Vue.use(Router);
+
+const routes = [
+  { path: "/", component: Inicio },
+  { path: "/login", component: Login },
+  { path: "/registro", component: Registro }
+];
+
+const router = new Router({
+  routes
+});
--- gimnasio/src/router/index.js
+++ gimnasio/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Inicio from '../views/Inicio.vue'
 
+
 const routes = [
   {
     path: '/',
